fc88958b24bce00442796b09a5c87c8c5afa4c9b
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-08-20 13:13:11 +0100

    created global style

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import Fonts from "./fonts";
+import variables from "./variables";
+
+const GlobalStyle = createGlobalStyle`
+${Fonts};
+${variables};
+
+* {
+box-sizing: border-box;
+padding: 0;
+margin: 0;
+scroll-behavior: smooth;
+font-family: var(--font-custom);
+font-weight: 400;
+line-height: 1.5;
+}
+
+`;
+
+export default GlobalStyle;
