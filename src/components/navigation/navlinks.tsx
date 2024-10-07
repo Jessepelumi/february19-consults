502584d6d636b7faf9ed649cab928a93be858fa5
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-07 16:35:11 +0100

    navigation links component

---
+++ src/components/navigation/navlinks.tsx
@@ -0,0 +1,13 @@
+import { NavlinksStyle } from "@/styles/components/navlinks.style";
+
+export default function NavLinks() {
+  return (
+    <NavlinksStyle>
+      <ul>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact Us</li>
+      </ul>
+    </NavlinksStyle>
+  );
+}
